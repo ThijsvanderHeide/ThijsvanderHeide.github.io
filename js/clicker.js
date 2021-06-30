128a6a893aba1fa7712b0dbdc48322ea253b0671
ZombieKiller <thijsvdheide@yahoo.com>
2021-06-30 21:39:14 +0200

    Clicker Update

--- js/clicker.js
+++ js/clicker.js
@@ -1,24 +1,48 @@
 const clickHere = document.querySelector("h1");
-const upgradeHere = document.querySelector("p");
+const upgradeHere = document.querySelector(".upgrade");
+const upgradeHereAuto = document.querySelector(".upgrade2");
+const numberCount = document.querySelector(".counter");
 
 let clicks = 0;
 let newClicks = 1;
-let upgradedClick = false;
+let autoUpgrade = false;
 
 clickHere.addEventListener("click", clickAdd);
 upgradeHere.addEventListener("click", clickUpgrade)
+upgradeHereAuto.addEventListener("click", clickUpgradeAuto)
 
 function clickAdd() {
     clicks = clicks + newClicks;
-    console.log(clicks);  
+    numberCount.textContent = clicks;
 }
 
 function clickUpgrade() {
     if (clicks >= 10) {
         clicks = clicks - 10;
-        newClicks = newClicks + newClicks;
+        newClicks = newClicks + 1;
+        numberCount.textContent = clicks;
     }
     else{
         alert("Je hebt niet genoeg clicks");
     }
 }
+
+function clickUpgradeAuto() {
+    if (clicks >= 100) {
+        clicks = clicks - 100;
+        autoUpgrade = true;
+        numberCount.textContent = clicks;
+    }
+    else{
+        alert("Je hebt niet genoeg clicks");
+    }
+}
+
+function autoClicks() {
+    if (autoUpgrade == true) {
+        clicks = clicks + 1;
+        numberCount.textContent = clicks;
+    }
+}
+
+setInterval(autoClicks, 1000);
