f0e09fe9f5f1e1ca4804142a16b1750c609815e5
ZombieKiller <thijsvdheide@yahoo.com>
2021-10-06 19:54:33 +0200

    Clicker Ge-update

--- js/clicker.js
+++ js/clicker.js
@@ -3,11 +3,14 @@ const upgradeHere = document.querySelector(".upgrade-button");
 const upgradeHereAuto = document.querySelector(".auto-button");
 const fungrade = document.querySelector(".fun-button");
 const numberCount = document.querySelector(".counter");
-const flickHere = document.querySelector(".flick-button")
-const flickAuto = document.querySelector(".flick-auto-button")
-const flickCount = document.querySelector(".flick-counter")
+const flickHere = document.querySelector(".flick-button");
+const flickAuto = document.querySelector(".flick-auto-button");
+const flickCharged = document.querySelector(".flick-charge-button");
+const flickCount = document.querySelector(".flick-counter");
+const scrollPress = document.querySelector(".reset-button");
+const scrollCounter = document.querySelector(".scroll-count")
 
-let buttonBox = [clickHere, upgradeHere, upgradeHereAuto, fungrade];
+let buttonBox = [clickHere, upgradeHere, upgradeHereAuto, fungrade, flickHere, flickAuto, flickCharged, scrollPress];
 
 let clicks = 0;
 let newClicks = 1;
@@ -18,18 +21,24 @@ let autoCost = 100;
 let funRequired = 1000;
 let autoUpgrade = false;
 let autoFlickUpgrade = false;
+let autoChargeUpgrade = false;
+let newScroll = 0;
+let miniScroll = 1;
 
 let flicks = 0;
+let autoSetFlicks = 0;
 
 clickHere.addEventListener("click", clickAdd);
 upgradeHere.addEventListener("click", clickUpgrade);
 upgradeHereAuto.addEventListener("click", clickUpgradeAuto);
 fungrade.addEventListener("click", funStart);
-flickHere.addEventListener("click", flickAdd)
-flickAuto.addEventListener("click", flickAutoAdd)
+flickHere.addEventListener("click", flickAdd);
+flickAuto.addEventListener("click", flickAutoAdd);
+flickCharged.addEventListener("click", flickSuperCharge);
+scrollPress.addEventListener("click", scrollAddition);
 
 function clickAdd() {
-    clicks = clicks + newClicks;
+    clicks = clicks + newClicks * miniScroll;
     numberCount.textContent = clicks;
 }
 
@@ -50,9 +59,12 @@ function clickUpgrade() {
 function clickUpgradeAuto() {
     if (clicks >= autoCost) {
         clicks = clicks - autoCost;
-        autoCost = autoCost + autoCost;
+        autoCost = autoCost * 1.7;
+        autoCost = Math.round(autoCost);
         autoUpgrade = true;
-        autoSetClicks++;
+        autoSetClicks++
+        autoSetClicks = autoSetClicks * 1.3;
+        autoSetClicks = Math.round(autoSetClicks);
         numberCount.textContent = clicks;
         upgradeHereAuto.innerHTML = "Here for automated clicks (Cost: " + autoCost + " Clicks)";
     }
@@ -103,7 +115,7 @@ function flickAdd() {
         flickCount.textContent = flicks;
     }
     else {
-        alert("Je hebt niet genoeg clicks")
+        alert("Je hebt niet genoeg flicks")
     }
 }
 
@@ -111,6 +123,18 @@ function flickAutoAdd() {
     if (flicks >= 10) {
         flicks = flicks - 10;
         autoFlickUpgrade = true;
+        flickCount.textContent = flicks;
+        autoSetFlicks++
+    }
+    else{
+        alert("Je hebt niet genoeg flicks");
+    }
+}
+
+function flickSuperCharge() {
+    if (flicks >= 100) {
+        flicks = flicks - 100;
+        autoChargeUpgrade = true;
         flickCount.textContent = flicks;
     }
     else{
@@ -120,10 +144,36 @@ function flickAutoAdd() {
 
 function autoFlicks() {
     if (autoFlickUpgrade == true) {
-        flicks = flicks + 1;
+        flicks = flicks + autoSetFlicks;
         flickCount.textContent = flicks;
+    }
+}
+function autoClicksCharged() {
+    if (autoChargeUpgrade == true) {
+        clicks = clicks + 100;
+        numberCount.textContent = clicks;
+    }
+}
+
+function scrollAddition() {
+    if (clicks > 13000 && flicks > 1300) {
+    clicks = 0;
+    flicks = 0;
+    autoSetFlicks = 0;
+    autoSetClicks = 0;
+    autoCost = 0;
+    upgradeCost = 0;
+    newScroll = newScroll + 1;
+    numberCount.textContent = clicks;
+    flickCount.textContent = flicks;
+    scrollCounter.textContent = "Scrolls: " + newScroll;
+    miniScroll++
+    }
+    else{
+        alert("Je hebt 13000 Clicks en 1300 Flicks nodig voor 1 Scroll");
     }
 }
 
 setInterval(autoClicks, 1000);
+setInterval(autoClicksCharged, 500);
 setInterval(autoFlicks, 1000);
